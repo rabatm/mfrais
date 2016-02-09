0d2e69b1ae46510691ecc6dd597b8fd013d45b49
Martin <martin.rabat@gmail.com>
2016-02-09 23:57:22 +0100

    Index.html : mise en forme avec bootsrap ajout de tableaux : 	un pour les frais client 	un pour les frai pro ajout d'une nav (a modifier par la suite)
    debut de la mise en page (a modifier peu etre)
    
    ajout dun formulaire pour lajout de nouvel ndf
    
    mise ne place d'angularjs:
    	initialisation de l app
    	ajout de donnee test

---
+++ js/app.js
@@ -0,0 +1,20 @@
+angular.module("mFrais", [])
+.controller("mFraisCtrl", function($scope){
+	
+	$scope.frais = [
+		{ value: "CLIENT", label: 'Frais client', transactions: [
+			{ id: 1, type: "ndf", date: "20/03/2014", description: "dec 2014", justificatif: "tex.jgp", montant:"228,90"},
+			{ id: 2, type: "rmb", date: "20/03/2014", description: "Virment", justificatif: "n 222", montant:"228,90"},
+			{ id: 3, type: "ndf", date: "20/03/2014", description: "dec 2014", justificatif: "tex.jgp", montant:"228,90"}
+			]
+		},
+		{ value: "PRO", label: 'Frais PRO', transactions: [
+			{ id: 1, type: "ndf",date: "08/01/2016", description: "Frais Janvier 2016", justificatif: "tex.jgp", montant:"600,90"},
+			{ id: 2, type: "rmb",date: "22/01/2016", description: "Virement", justificatif: "tex.jgp", montant:"600,90"},
+			{ id: 3, type: "ndf",date: "05/02/2016", description: "Frais Février 2016", justificatif: "tex.jgp", montant:"610,90"}
+			]
+		}
+
+	];
+
+});
