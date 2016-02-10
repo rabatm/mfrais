2451ec717686c314cdd36412e3c0824bb09f87fc
Martin <martin.rabat@gmail.com>
2016-02-10 01:16:27 +0100

    modification apps.js

--- js/app.js
+++ js/app.js
@@ -3,18 +3,30 @@ angular.module("mFrais", [])
 	
 	$scope.frais = [
 		{ value: "CLIENT", label: 'Frais client', transactions: [
-			{ id: 1, type: "ndf", date: "20/03/2014", description: "dec 2014", justificatif: "tex.jgp", montant:"228,90"},
-			{ id: 2, type: "rmb", date: "20/03/2014", description: "Virment", justificatif: "n 222", montant:"228,90"},
-			{ id: 3, type: "ndf", date: "20/03/2014", description: "dec 2014", justificatif: "tex.jgp", montant:"228,90"}
+			{ id: 1, type: "ndf", date: "20/03/2014", description: "dec 2014", justificatif: "tex.jgp", montant:228.90},
+			{ id: 2, type: "rmb", date: "20/03/2014", description: "Virment", justificatif: "n 222", montant:228.90},
+			{ id: 3, type: "ndf", date: "20/03/2014", description: "dec 2014", justificatif: "tex.jgp", montant:228.90},
+			{ id: 2, type: "rmb", date: "20/03/2014", description: "Virment", justificatif: "n 222", montant:228.90},
 			]
 		},
-		{ value: "PRO", label: 'Frais PRO', transactions: [
-			{ id: 1, type: "ndf",date: "08/01/2016", description: "Frais Janvier 2016", justificatif: "tex.jgp", montant:"600,90"},
-			{ id: 2, type: "rmb",date: "22/01/2016", description: "Virement", justificatif: "tex.jgp", montant:"600,90"},
-			{ id: 3, type: "ndf",date: "05/02/2016", description: "Frais Février 2016", justificatif: "tex.jgp", montant:"610,90"}
+		{ value: "PRO", label: 'Frais Pro', transactions: [
+			{ id: 1, type: "ndf",date: "08/01/2016", description: "Frais Janvier 2016", justificatif: "tex.jgp", montant: 600.90},
+			{ id: 2, type: "rmb",date: "22/01/2016", description: "Virement", justificatif: "tex.jgp", montant: 600.90},
+			{ id: 3, type: "ndf",date: "05/02/2016", description: "Frais Février 2016", justificatif: "tex.jgp", montant:610.90}
 			]
 		}
 
 	];
-
+	$scope.countTotalEnt = function (ent) {
+		$total = 0;
+		ent.forEach(function(item) {
+				if (item.type == "ndf")
+					{$total += item.montant }
+				else
+					{$total -= item.montant }		
+				})
+		$scope.ClientR=100;
+		$scope.ProR=$total;
+		return $total;
+	}
 });
